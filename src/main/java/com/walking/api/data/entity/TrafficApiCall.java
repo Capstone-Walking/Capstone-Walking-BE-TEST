7af855dd5e29fcfdce805dfe5f8a51aa5b30e3c6
nove1080 <skwpqjq@gmail.com>
2024-05-04 16:41:34 +0900

    feat: TrafficApiCall 추가
    <description>
    신호제어기 잔여시간 정보 API의 응답결과를 나타내는 엔티티
    <issue-number>

---
+++ src/main/java/com/walking/api/data/entity/TrafficApiCall.java
@@ -0,0 +1,47 @@
+package com.walking.api.data.entity;
+
+import com.walking.api.data.constant.TrafficColor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/** 신호제어기 잔여시간 정보 서비스의 응답 결과 */
+@Entity
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrafficApiCall {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// 신호등
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Traffic traffic;
+
+	// 신호등 색상
+	@Enumerated(value = EnumType.STRING)
+	@Column(nullable = false)
+	private TrafficColor color;
+
+	// 잔여시간
+	@Column(nullable = false)
+	private Double timeLeft;
+
+	// executionNumber 가 클수록 최근에 API call 을 통해 받아온 데이터
+	// 실제 프로젝트에는 JobExecutionId로 대체할 부분
+	@Column(nullable = false)
+	private Integer executionNumber;
+}
