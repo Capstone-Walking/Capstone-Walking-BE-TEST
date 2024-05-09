0d547788403fd010631d9ca18ca1074f4a37d2b4
belljun3395 <195850@jnu.ac.kr>
2024-05-09 16:30:24 +0900

    feat: PredictData 구현
    <description>
    PredictDatum의 래핑 객체입니다.

--- src/main/java/com/walking/api/service/dto/PredictData.java
+++ src/main/java/com/walking/api/service/dto/PredictData.java
@@ -1,59 +1,46 @@
 package com.walking.api.service.dto;
 
 import com.walking.api.data.entity.Traffic;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-/** 예측한 결과를 나타내는 클래스 */
 @Getter
-@AllArgsConstructor
 @ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 public class PredictData {
 
-	private Traffic traffic;
-	private Optional<Float> redCycle;
-	private Optional<Float> greenCycle;
+	private List<PredictDatum> predictData;
 
-	public PredictData(Traffic traffic) {
-		this.traffic = traffic;
-		redCycle = Optional.empty();
-		greenCycle = Optional.empty();
+	@Builder
+	public PredictData(Map<Traffic, PredictDatum> predictData) {
+		this.predictData = new ArrayList<>(predictData.values());
 	}
 
 	/**
-	 * 빨간불의 사이클이 계산 되었는지 반환합니다.
+	 * 예측을 수행한 결과를 읽어보고 예측이 아직 끝나지 않은 신호등 리스트를 반환합니다.
 	 *
-	 * @return redCycle 이 존재하면 true
+	 * @param prePredictData 예측을 수행한 결과
+	 * @return 신호등 리스트
 	 */
-	public boolean isPredictedRedCycle() {
-		return redCycle.isPresent();
+	public PredictData refresh(Map<Traffic, PredictDatum> prePredictData) {
+		return PredictData.builder().predictData(prePredictData).build();
 	}
 
-	/**
-	 * 초록불의 사이클이 계산 되었는지 반환합니다.
-	 *
-	 * @return greenCycle 이 존재하면 true
-	 */
-	public boolean isPredictedGreenCycle() {
-		return greenCycle.isPresent();
+	public boolean isEmpty() {
+		return predictData.isEmpty();
 	}
 
-	/**
-	 * 예측이 끝났는지 여부를 반환합니다.
-	 *
-	 * @return 예측이 끝났으면 true, 아니면 false
-	 */
-	public boolean isComplete() {
-		return isPredictedRedCycle() && isPredictedGreenCycle();
-	}
-
-	public void updateRedCycle(Optional<Float> redCycle) {
-		this.redCycle = redCycle;
-	}
-
-	public void updateGreenCycle(Optional<Float> greenCycle) {
-		this.greenCycle = greenCycle;
+	public List<Traffic> getTraffics() {
+		return predictData.stream().map(PredictDatum::getTraffic).collect(Collectors.toList());
 	}
 }
