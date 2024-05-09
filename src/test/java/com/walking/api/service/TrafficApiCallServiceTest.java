24e34b5e0deb6acdc1d032c23a744068e85b0075
belljun3395 <195850@jnu.ac.kr>
2024-05-09 10:04:39 +0900

    refactor: 여러 번의 테스트가 필요한 경우 ParameterizedTest로 변경합니다

--- src/test/java/com/walking/api/service/TrafficApiCallServiceTest.java
+++ src/test/java/com/walking/api/service/TrafficApiCallServiceTest.java
@@ -2,6 +2,8 @@ package com.walking.api.service;
 
 import com.walking.api.ApiApp;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -20,8 +22,11 @@ class TrafficApiCallServiceTest {
 
 	@Rollback(false)
 	@Transactional
-	void 신호_색상과_잔여시간_데이터를_생성한다() {
-		executionNumber = 2; // traffic_api_call 테이블에 존재하는 마지막 execution_number 의 + 1된 값으로 실행시켜야합니다.
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20})
+	void 신호_색상과_잔여시간_데이터를_생성한다(int number) {
+		executionNumber =
+				number; // traffic_api_call 테이블에 존재하는 마지막 execution_number 의 + 1된 값으로 실행시켜야합니다.
 		trafficApiCallService.execute(60, executionNumber);
 	}
 }
