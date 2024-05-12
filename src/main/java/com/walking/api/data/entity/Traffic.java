ad34a2a56c0be130b84d7565afa05949ac8a33ed
belljun3395 <195850@jnu.ac.kr>
2024-05-12 15:13:51 +0900

    feat: Traffic에 @EqualsAndHashCode를 추가합니다

--- src/main/java/com/walking/api/data/entity/Traffic.java
+++ src/main/java/com/walking/api/data/entity/Traffic.java
@@ -3,8 +3,10 @@ package com.walking.api.data.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /** 신호등 */
+@EqualsAndHashCode
 @Entity
 @Data
 public class Traffic {
