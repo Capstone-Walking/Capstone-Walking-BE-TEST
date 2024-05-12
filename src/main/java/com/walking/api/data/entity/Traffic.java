56ec346a0f8bbdbc1806ca48c81cf77f8e625923
belljun3395 <195850@jnu.ac.kr>
2024-05-13 00:10:08 +0900

    feat: Traffic 생성자 추가

--- src/main/java/com/walking/api/data/entity/Traffic.java
+++ src/main/java/com/walking/api/data/entity/Traffic.java
@@ -2,13 +2,17 @@ package com.walking.api.data.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /** 신호등 */
 @EqualsAndHashCode
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Traffic {
 
 	@Id private Long id;
